e4f0b9c3a86f2a107f423462fda833b8523f4dd3
Emmanuel Boateng <emmanuelkwakuboateng50@gmail.com>
2025-02-05 23:13:27 -0500

    Add files via upload

---
+++ scripts/email-validator.js
@@ -0,0 +1,65 @@
+// Algorithm for E-mail Validator
+// 1, What element runs the validation function? (submit-button)
+// 2, Where do I want to display the validation results? (beneath the submit-button)
+// 3, What tag/element displays the results? (a span element)
+
+const initialState =  "Initializing...";
+const validationCheck =  "Email validation check in progress please wait...";
+const validationResult = "Good news! This email is valid and verified.";
+const errorMessageDisplay =  "email required before validation";
+const submitBtn = document.getElementById('submit-btn');
+
+// time-out function
+function validateEmail() {
+  const emailInput = document.getElementById('inputedEmail').value
+  
+  if (emailInput) {
+    setTimeout(() => {
+      const initialInfo = document.getElementById('validation-info');
+      initialInfo.textContent = initialState;
+      initialInfo.style.color = "#fff";
+    }, 2000)
+
+    setTimeout(() => {
+      const emailPreview = document.getElementById('display-email');
+      emailPreview.textContent = `Email Preview: ${emailInput}`;
+    }, 10000)
+
+    setTimeout(() => {
+      const progressResult = document.getElementById('validation-info');
+      progressResult.textContent = validationCheck;
+      progressResult.style.color = "#fff";
+    }, 30000)
+
+    setTimeout(() => {
+      const displayLine = document.getElementById('validation-info');
+      displayLine.textContent = validationResult;
+      displayLine.style.color = "green";
+    }, 50000)
+
+    setTimeout(() => {
+      alert('Validation done.');
+    }, 52000)
+
+  } else if (!emailInput) {
+    revokeValidation();
+  }
+}
+
+// fuction that handles onkeydown event with keyboard Enter key
+function handleValidateEnterKey(event) {
+  if (event.key === 'Enter') { 
+    validateEmail();
+  }
+} 
+
+function revokeValidation() {
+  const errorMessage = document.getElementById('validation-info');
+  errorMessage.textContent = errorMessageDisplay;
+  errorMessage.style.color = "red";
+  return;
+}
+
+submitBtn.addEventListener('click', validateEmail);
+
+
